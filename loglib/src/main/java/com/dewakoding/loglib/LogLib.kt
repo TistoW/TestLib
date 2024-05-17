9d8d0b1b140e031f56e47205ed27d38915781b69
tisto <imtisto@gmail.com>
2024-05-18 00:09:53 +0700

    - ada logs

--- loglib/src/main/java/com/dewakoding/loglib/LogLib.kt
+++ loglib/src/main/java/com/dewakoding/loglib/LogLib.kt
@@ -2,21 +2,6 @@ package com.dewakoding.loglib
 
 import android.util.Log
 
-
-/**
-
-Created by
-name : Septiawan Aji Pradana
-email : septiawanajipradana@gmail.com
-website : dewakoding.com
-
- **/
-class LogLib {
-    companion object Builder {
-        private const val TAG = "LOG_LIB"
-
-        fun run(message: String) {
-            Log.i(TAG, message)
-        }
-    }
+fun logD(message: String) {
+    Log.d("zenenta", message)
 }
